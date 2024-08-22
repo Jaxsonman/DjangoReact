3968514bb477ab20d479ee6bb39b707695ca7efd
Jaxson Mansouri <mansourijaxson@gmail.com>
2024-08-21 22:29:51 -0600

    static change

--- djangoreact/settings.py
+++ djangoreact/settings.py
@@ -118,10 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/frontend/static/'
+# URL for serving static files
+STATIC_URL = '/static/'
+
+# Directory to collect static files for deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional directories to look for static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build/static'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),  # React build output
 ]
 
 # Default primary key field type
