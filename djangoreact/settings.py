7b1a5b21c32cc14ecd3daa5e76e2158f3a9a7784
Jaxson Mansouri <mansourijaxson@gmail.com>
2024-08-22 18:13:32 -0600

    static

--- djangoreact/settings.py
+++ djangoreact/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Directory to collect static files for deployment
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Additional directories to look for static files
 STATICFILES_DIRS = [
